b45c22c2346416878e51617ffb91d4d48c3e8bab
MSonny68 <maik.sonnenberg@hotmail.degit config --global user.name MSonny68git loggit config --global user.email maik.sonnenberg@hotmail.de>
2022-12-01 09:30:38 +0100

    klausurbesprechung

--- funktionen.js
+++ funktionen.js
@@ -2,35 +2,90 @@
 
 /* Primitives */
 
-// let a // (var) Definition oder Deklaration
+
+
+let a; // (var) Definition oder Deklaration
 
 // Wertzuweisung / Assignment
+// a = 2;
+// a = "Hallo";
+// console.log(typeof a);
+// console.log(a);
 
+// /* Arrays */
+// // Navigation über INDEX
 
-/* Arrays */
-// Navigation über INDEX
+// let arr;
 
+// arr =[3,5,7,9];
+// console.log(arr);
+// console.log(arr[0]);  // erstes Element
+// console.log(arr[arr.length-1]); //letztes Element
 
 
 
 /* Array -Info, nicht klausurrelevant*/
+// arr = [];
+// console.log(arr);
+// arr.push("a");
+// console.log(arr);
+// arr.push("b");
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
 
+// /* Daten-Objekte */
+// // Navigation über Bezeichner (keys)
 
-/* Daten-Objekte */
-// Navigation über Bezeichner (keys)
+// let obj = {x:"Hi", y:20};
+// console.log(obj);
+// console.log(obj.x);
 
+// obj = {
+//     val0: 10,
+//     sub1:{
+//         val1: "hi",
+//         val2: 2
+//     }
+// };
 
-
+//console.log(obj);
  /*DOM*/
 
 
+//   console.log(document);
+// console.log(document.body);
+// console.log(document.body.children);
+// console.log(document.body.children[0]);
+// console.log(document.body.children[0].innerHTML);
+// document.body.children[0].innerText = "Hi";
+// console.log(document.getElementById("test"));
+// document.getElementById("test").innerHTML = "Hallo Text!"
+
 /* ENTSCHEIDUNGSSTRUKTUREN | CONTROL FLOW*/
 
+// 
 
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
 
  //   https://github.com/01-PP/03_Entscheidungsstrukturen
 
 /* FUNKTIONEN */
+
+function test() {
+    console.log("Testausgabe")
+}
+test();
+
 
 
 // 3. Parameter 
